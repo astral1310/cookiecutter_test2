0cd7ce833036646590714835321460a503a9e454
César A <cesar1310@gmail.com>
2022-07-01 11:02:38 -0500

    Update setup.R

--- {{cookiecutter.directory_name}}/setup.R
+++ {{cookiecutter.directory_name}}/setup.R
@@ -2,12 +2,6 @@ if (!requireNamespace('here'))
   install.packages('here')
 library('here')
 
-if (!requireNamespace('renv'))
-  install.packages('renv')
-library('renv')
-
-renv::init()
-
 packages <- scan(here('requirements.txt'), what="", sep='\n')
-source(here('src','utils','install_load_packages.R'))
+source(here('install_load_packages.R'))
 install_load_packages(packages)
